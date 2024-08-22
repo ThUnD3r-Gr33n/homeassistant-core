db3fb8f4d686f5217fd437a1e45bc20449b1865b
Marc Brooks <IDisposable@gmail.com>
2024-08-22 17:40:55 -0500

    Add explicit pH unit
    Make ph (potential of hydrogen) a proper unit

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -1170,6 +1170,8 @@ PERCENTAGE: Final = "%"
 # Rotational speed units
 REVOLUTIONS_PER_MINUTE: Final = "rpm"
 
+# pH units
+POTENTIAL_OF_HYDROGEN: Final = "pH"
 
 # Irradiance units
 class UnitOfIrradiance(StrEnum):
