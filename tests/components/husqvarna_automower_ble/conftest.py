a9d2908dcdf23daf01c08828b7b92c7d01829394
Joostlek <joostlek@outlook.com>
2024-09-01 13:31:17 +0200

    Clean up, add strings

--- tests/components/husqvarna_automower_ble/conftest.py
+++ tests/components/husqvarna_automower_ble/conftest.py
@@ -8,25 +8,12 @@ import pytest
 
 from homeassistant.components.husqvarna_automower_ble.const import DOMAIN
 from homeassistant.components.husqvarna_automower_ble.coordinator import SCAN_INTERVAL
-from homeassistant.const import CONF_ADDRESS, CONF_CLIENT_ID, CONF_UNIQUE_ID
+from homeassistant.const import CONF_ADDRESS, CONF_CLIENT_ID
 from homeassistant.core import HomeAssistant
 
 from . import AUTOMOWER_SERVICE_INFO
 
 from tests.common import MockConfigEntry, async_fire_time_changed
-
-
-@pytest.fixture
-def mock_entry() -> MockConfigEntry:
-    """Create hass config fixture."""
-    return MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_ADDRESS: AUTOMOWER_SERVICE_INFO.address,
-            CONF_UNIQUE_ID: AUTOMOWER_SERVICE_INFO.address,
-            CONF_CLIENT_ID: 1197489078,
-        },
-    )
 
 
 @pytest.fixture
@@ -55,6 +42,7 @@ async def scan_step(
 
     return delay
 
+
 @pytest.fixture(autouse=True)
 def mock_automower_client(enable_bluetooth: None, scan_step) -> Generator[AsyncMock]:
     """Mock a BleakClient client."""
@@ -79,29 +67,6 @@ def mock_automower_client(enable_bluetooth: None, scan_step) -> Generator[AsyncM
 
         yield client
 
-@pytest.fixture
-def airgradient_devices(
-    mock_automower_client: AsyncMock, request: pytest.FixtureRequest
-) -> Generator[AsyncMock]:
-    """Return a list of AirGradient devices."""
-    return mock_automower_client
-
-
-@pytest.fixture
-def mock_new_airgradient_client(
-    mock_automower_client: AsyncMock,
-) -> AsyncMock:
-    """Mock a new AirGradient client."""
-    return mock_automower_client
-
-
-@pytest.fixture
-def mock_cloud_airgradient_client(
-    mock_automower_client: AsyncMock,
-) -> AsyncMock:
-    """Mock a cloud AirGradient client."""
-    return mock_automower_client
-
 
 @pytest.fixture
 def mock_config_entry() -> MockConfigEntry:
@@ -111,63 +76,7 @@ def mock_config_entry() -> MockConfigEntry:
         title="Husqvarna AutoMower",
         data={
             CONF_ADDRESS: AUTOMOWER_SERVICE_INFO.address,
-            CONF_UNIQUE_ID: AUTOMOWER_SERVICE_INFO.address,
             CONF_CLIENT_ID: 1197489078,
         },
-        unique_id="84fce612f5b8",
+        unique_id=AUTOMOWER_SERVICE_INFO.address,
     )
-
-
-# class MockMower:
-#     """Mock BleakClient."""
-
-#     def __init__(self, *args, **kwargs) -> None:
-#         """Mock BleakClient."""
-
-#     async def __aexit__(self, *args, **kwargs):
-#         """Mock BleakClient.__aexit__."""
-
-#     async def connect(self, *args, **kwargs) -> bool:
-#         """Mock BleakClient.connect."""
-#         return True
-
-#     async def disconnect(self, *args, **kwargs):
-#         """Mock BleakClient.disconnect."""
-
-#     def is_connected(self):
-#         """Mock BleakClient.is_connected."""
-#         return True
-
-#     async def get_model(self) -> str:
-#         """Mock BleakClient.get_model."""
-#         return "305"
-
-#     async def battery_level(self) -> int:
-#         """Mock BleakClient.battery_level."""
-#         return 100
-
-#     async def mower_state(self) -> str:
-#         """Mock BleakClient.mower_state."""
-#         return "pendingStart"
-
-#     async def mower_activity(self) -> str:
-#         """Mock BleakClient.mower_activity."""
-#         return "charging"
-
-#     async def probe_gatts(self, device):
-#         """Mock BleakClient.probe_gatts."""
-#         return ("Husqvarna", "Automower", "305")
-
-
-# @pytest.fixture(autouse=True)
-# def mock_client(enable_bluetooth: None, scan_step) -> None:
-#     """Auto mock bluetooth."""
-
-#     with (
-#         patch(
-#             "homeassistant.components.husqvarna_automower_ble.config_flow.Mower",
-#             MockMower,
-#         ),
-#         patch("homeassistant.components.husqvarna_automower_ble.Mower", MockMower),
-#     ):
-#         yield MockMower
