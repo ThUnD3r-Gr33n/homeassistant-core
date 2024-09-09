1e87b38399b725bb2142d0993920ae5f254743c3
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-09-09 21:34:06 +0200

    Add OperationalState attributes from Matter OperationalState cluster

--- homeassistant/components/matter/sensor.py
+++ homeassistant/components/matter/sensor.py
@@ -53,6 +53,14 @@ AIR_QUALITY_MAP = {
 }
 
 
+OPERATIONAL_STATE_MAP = {
+    clusters.OperationalState.Enums.OperationalStateEnum.kStopped : "Stopped",
+    clusters.OperationalState.Enums.OperationalStateEnum.kRunning: "Running ",
+    clusters.OperationalState.Enums.OperationalStateEnum.kPaused: "Paused ",
+    clusters.OperationalState.Enums.OperationalStateEnum.kError: "Error",
+}
+
+
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
@@ -557,5 +565,21 @@ DISCOVERY_SCHEMAS = [
         required_attributes=(
             clusters.ElectricalEnergyMeasurement.Attributes.CumulativeEnergyImported,
         ),
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.SENSOR,
+        entity_description=MatterSensorEntityDescription(
+            key="OperationalState",
+            #native_unit_of_measurement=None,
+            device_class=SensorDeviceClass.ENUM,
+            #entity_category=EntityCategory.DIAGNOSTIC,
+            #state_class=SensorStateClass.MEASUREMENT,
+            state_class=None,
+            # convert to set first to remove the duplicate unknown value
+            options=list(set(OPERATIONAL_STATE_MAP.values())),
+            measurement_to_ha=lambda x: OPERATIONAL_STATE_MAP[x],
+        ),
+        entity_class=MatterSensor,
+        required_attributes=(clusters.OperationalState.Attributes.OperationalState,),
     ),
 ]
